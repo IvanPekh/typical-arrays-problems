af8c5d1c55f3d207cf6dfcd8abead2a317cce4cb
Yliya Pehota <pehotaig@gmail.com>
2020-09-30 16:38:52 +0300

    feat: add 3 functions: max, min, avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (Array.isArray(array) && array.length) { 
+    return array.reduce(function (acc, curr) {
+      return ( acc < curr ? acc : curr);
+    });
+  } else {
+    return 0;
+  }  
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (Array.isArray(array) && array.length) { 
+    return array.reduce(function (acc, curr) {
+      return ( acc > curr ? acc : curr);
+    });
+  } else {
+    return 0;
+  }  
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (Array.isArray(array) && array.length) { 
+    return array.reduce(function (acc, curr) {
+      return acc + curr; 
+    }) / array.length;
+  } else {
+    return 0;
+  }  
 }
